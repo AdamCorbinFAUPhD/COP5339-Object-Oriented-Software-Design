4e59256dff116539e2e3230436da0087721de0b4
Adam Corbin <acorbin3@gmail.com>
2020-03-17 05:42:00 -0400

    Starting 5.3

---
+++ Homeworks/Homework4/q5.3/src/main/GUIView.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class GUIView {
+    JFrame frame = new JFrame();
+    JPanel textPanel = new JPanel();
+    JPanel barPanel = new JPanel();
+    GUIView(){
+
+    }
+
+}
