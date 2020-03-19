b8dfc4514f095110e03e5f367498ca921965c00e
Adam Corbin <acorbin3@gmail.com>
2020-03-19 03:55:14 -0400

    Adding q6 base

--- Homeworks/Project/create_pdf.bat
+++ Homeworks/Project/create_pdf.bat
@@ -1,1 +1,1 @@
-pandoc --toc .\report.md -o final_report.pdf -f markdown+implicit_figures+link_attributes --filter pandoc-mermaid --mathjax --listings -H listings-setup.tex
+pandoc  --verbose --toc .\report.md -o final_report.pdf -f markdown+implicit_figures+link_attributes -F mermaid-filter.cmd --mathjax --listings -H listings-setup.tex
